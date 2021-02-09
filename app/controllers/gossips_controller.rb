7e5a4b78510199479e9ca2f1974d7d764e3bb99d
Caro407 <caro.lefevre407@hotmail.fr>
2021-02-09 18:23:32 +0100

    alerts for success or fail saving of new gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,23 +10,31 @@ class GossipsController < ApplicationController
     @gossip = Gossip.new
   end
 
+  def create_params
+    params.permit(:user, :content, :title)
+  end
+
   def create
     pp params
-    #1ère étape : récupérer les params et vérifier que tout roule
-    user = User.find_by(first_name: params[:user])
+    #1ère étape : récupérer les params
+    user = User.find_by(first_name: create_params[:user])
 
+    #2ème étape : on prépare la nouvelle instance
+    @gossip = Gossip.new(user: user, content: create_params[:content], title: create_params[:title])
+
+    #2ème étape bis: on vérifie que tout roule
     if user == nil
+      flash[:warning] = "Echec lors de la création du potin : merci de recommencer."
       render :new
       return
     end
 
-    #2ème étape : on prépare la nouvelle instance
-    @gossip = Gossip.new(user_id: user.id, content: params[:content], title: params[:title])
-
     #3ème étape : save and redirect
     if @gossip.save
       redirect_to gossips_path
+      flash[:success] = "Potin créé !"
     else
+      flash[:warning] = "Echec : " + @gossip.errors.full_messages.join(" ")
       render :new
       return
     end
